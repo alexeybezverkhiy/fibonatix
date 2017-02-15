e534b93535529143c7d454ab732a41fad6537f84
snark013 <alexandre.g.petrov@gmail.com>
2017-02-15 17:25:57 +0300

    ChangeLog: - add Model files (primitives) for Status request, - add template for Start request to Backend, - use Web Cache for storing data between Sale and Status request, - some other changes.

---
+++ Merchant/MerchantAPI/MerchantAPI/CommDoo/BackEnd/Requests/Start.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace MerchantAPI.CommDoo.BackEnd.Requests
+{
+    public class Start
+    {
+    }
+}
