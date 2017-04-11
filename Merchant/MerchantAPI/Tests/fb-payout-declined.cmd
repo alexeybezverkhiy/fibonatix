fd1d37d5ad05b70687ddd79cb0433cd4e5e7f0b9
Alexey Bezverkhiy <alexey.bezverkhiy@yandex.ru>
2017-04-11 11:32:03 +0300

    - add Windows commans script for testing

--- Merchant/MerchantAPI/Tests/fb-payout-declined.cmd
+++ Merchant/MerchantAPI/Tests/fb-payout-declined.cmd
@@ -3,7 +3,7 @@
 
 @set fileTraceLog=%~n0.trace.log
 @set url=http://%targetHost%:52380/paynet/api/v2/payout/250
-@set accountNumber=1234567890
+@set accountNumber=0987654321
 
 @set headerData=Authorization: OAuth realm="",oauth_version="1.0",oauth_consumer_key="merchantlogin",oauth_timestamp="1490718302",oauth_nonce="1eK7t1kIhTx",oauth_signature_method="HMAC-SHA1",oauth_signature="DDhwSEj2e7nZ2a07S5p%2Fizfe3Rg%3D"
 @set requestData=account_number=%accountNumber%^&amount=100^&bank_branch=test_branch^&bank_name=test_bank^&client_orderid=%paramClientOrderId%^&currency=USD
--- Merchant/MerchantAPI/Tests/setenv.cmd
+++ Merchant/MerchantAPI/Tests/setenv.cmd
@@ -5,6 +5,13 @@
 @set optProxy=--proxy localhost:8888
 
 @if "%1" == "skipAll" goto :eof
+@if "%1" == "skip2nd" goto :only1st
 
+:bothParam
 @if "%1" == "" (@set /p paramClientOrderId="client_orderid:") else (@set paramClientOrderId=%1)
 @if "%2" == "" (@set /p paramOrderId="orderid:") else (@set paramOrderId=%2)
+goto :eof
+
+:only1st
+@set /p paramClientOrderId="client_orderid:"
+goto :eof
