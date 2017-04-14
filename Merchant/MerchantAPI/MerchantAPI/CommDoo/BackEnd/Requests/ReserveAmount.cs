fe29a55616aed99cb8db65dcd628622aa704cdf8
snark013 <alexandre.g.petrov@gmail.com>
2017-04-14 14:14:27 +0300

    ChangeLog: - 'Account Verification' method added.

--- Merchant/MerchantAPI/MerchantAPI/CommDoo/BackEnd/Requests/ReserveAmount.cs
+++ Merchant/MerchantAPI/MerchantAPI/CommDoo/BackEnd/Requests/ReserveAmount.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.ComponentModel.DataAnnotations;
 using System.Xml;
 using System.Xml.Serialization;
+using MerchantAPI.Models;
+using MerchantAPI.Helpers;
+using MerchantAPI.Controllers.Factories;
 
 namespace MerchantAPI.CommDoo.BackEnd.Requests
 {
@@ -12,6 +15,52 @@ namespace MerchantAPI.CommDoo.BackEnd.Requests
     [XmlRoot("Request")]
     public class ReserveAmountRequest : StartRequest
     {
+        public static ReserveAmountRequest createRequestByModel(AccountVerificationRequestModel model, int endpointId, string fibonatixId) {
+            ReserveAmountRequest request = new ReserveAmountRequest() {
+                Client = new ClientData() {
+                    ClientID = WebApiConfig.Settings.GetClientID(endpointId),
+                    SharedSecret = WebApiConfig.Settings.GetSharedSecret(endpointId),
+                },
+                Payment = new PaymentData() {
+                    PaymentType = "CreditCard",
+                    Amount = "100",
+                    Currency = "EUR",
+                    ReferenceID = model.client_orderid + "-" + DateTime.Now.ToString("yyyyMMddHHmmss.fff"),
+                },
+                Customer = new CustomerData() {
+                    Person = new PersonData() {
+                        FirstName = model.first_name,
+                        LastName = model.last_name,
+                        DateOfBirth = Helpers.CommDooTargetConverter.ConvertBirthdayToString(model.birthday),
+                    },
+                    Address = new AddressData() {
+                        Street = model.address1,
+                        PostalCode = model.zip_code,
+                        City = model.city,
+                        State = model.state,
+                        Country = Helpers.CountryConverter.countryAlpha3String(model.country),
+                        PhoneNumber = model.phone,
+                        EmailAddress = model.email,
+                        IPAddress = model.ipaddress,
+                    },
+                    CreditCard = new CreditCardData() {
+                        CreditCardNumber = model.credit_card_number,
+                        CreditCardExpirationYear = model.expire_year.ToString(),
+                        CreditCardExpirationMonth = model.expire_month.ToString(),
+                        CreditCardValidationValue = model.cvv2.ToString(),
+                        CreditCardType = Helpers.CommDooTargetConverter.getCardType(model.credit_card_number),
+                    }
+                }
+            };
+            if( !String.IsNullOrEmpty(model.server_callback_url) ) {
+                request.Notification = new NotificationData() {
+                    NotificationURL = CommDooFrontendFactory.ResolveInternalNotificationUrl(endpointId, CommDooFrontendFactory.SUCC_EXTRA_PATH)
+                        + CommDooFrontendFactory.CreateNotifyParams(model.server_callback_url, fibonatixId),
+                };
+            }
+            return request;
+        }
+
         public override string executeRequest() {
             string requestURL = WebApiConfig.Settings.BackendServiceUrlMain + "/ReserveAmount";
             return sendRequest(requestURL);
---
+++ Merchant/MerchantAPI/MerchantAPI/Helpers/PostRequest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Net;
+using System.ComponentModel.DataAnnotations;
+
+namespace MerchantAPI.Helpers
+{
+    public class PostRequest
+    {
+
+        public void sendRequest(string url, byte[] request) {
+            try {
+                var ret = ProcessRequest(url, request);
+            } catch { }
+        }
+
+        public MemoryStream ProcessRequest(string url, byte[] request) {
+            var webRequest = CreateWebRequest(url, request);
+            return GetResponseStream(webRequest);
+        }
+
+        private WebRequest CreateWebRequest(string url, byte[] request) {
+            var webRequest = WebRequest.Create(url);
+            webRequest.Method = "POST";
+            webRequest.ContentType = "application/x-www-form-urlencoded";
+
+            byte[] data = request;
+            webRequest.ContentLength = data.Length;
+
+            var httpWebRequest = webRequest as HttpWebRequest;
+            if (httpWebRequest != null) {
+                httpWebRequest.UserAgent = String.Format("Fibonatix.CommDoo.WebGate {0}", this.GetType().Assembly.GetName().Version.ToString());
+                httpWebRequest.KeepAlive = false;
+            }
+
+            using (var requestStream = webRequest.GetRequestStream()) {
+                requestStream.Write(data, 0, data.Length);
+            }
+
+            return webRequest;
+        }
+
+        private MemoryStream GetResponseStream(WebRequest webRequest) {
+            WebResponse webResponse = null;
+            try {
+                webResponse = webRequest.GetResponse();
+                return Copy(webResponse.GetResponseStream());
+            } catch (WebException ex) {
+                Stream responseStream;
+                if (TryGetResponseDataFromWebException(ex, out responseStream)) {
+                    return Copy(responseStream);
+                }
+                throw ex;
+            } finally {
+                if (webResponse != null) {
+                    webResponse.Close();
+                }
+            }
+        }
+        private MemoryStream Copy(Stream stream) {
+            var memoryStream = new MemoryStream();
+            stream.CopyTo(memoryStream);
+            return memoryStream;
+        }
+        private bool TryGetResponseDataFromWebException(WebException webException, out Stream responseData) {
+            responseData = null;
+
+            var response = webException.Response as HttpWebResponse;
+            if (response == null) {
+                return false;
+            }
+
+            // Unprocessable Entity (The request was well-formed but was unable to be followed due to semantic errors.)
+            if (response.StatusCode == (HttpStatusCode)422) {
+                responseData = response.GetResponseStream();
+                response.Close();
+                return true;
+            }
+            return false;
+        }
+    }
+}
